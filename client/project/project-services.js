0f8b1c78ffff7e7ff56782bcf2b1bdc197d5f983
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-10-24 12:38:29 -0700

    projects page pass one complete

--- client/project/project-services.js
+++ client/project/project-services.js
@@ -5,47 +5,65 @@ angular.module('portfolio.projects-services', [])
 		{
 			name: 'Appception',
 			url: 'appception',
-			tags: ['javascript', 'angular', 'node', 'express', 'html', 'css', 'sass', 'ui', 'personal']
+			tags: ['javascript', 'angular', 'node', 'express', 'html', 'css', 'sass', 'ui', 'personal'],
+			year: '2014',
+			description: 'A web app that creates web apps'
 		},
 		{
 			name: 'Tate Art Explorer',
 			url: 'tate-art-explorer',
-			tags: ['javascript', 'angular', 'node', 'ionic', 'html', 'css', 'sass', 'personal']
+			tags: ['javascript', 'angular', 'node', 'ionic', 'html', 'css', 'sass', 'personal'],
+			year: '2014',
+			description: 'Browse the Tate Art Collection with ease.'
 		},
 		{
 			name: 'Github Scout',
 			url: 'github-scout',
-			tags: ['javascript', 'angular', 'node', 'express', 'html', 'css', 'sass', 'personal']
+			tags: ['javascript', 'angular', 'node', 'express', 'html', 'css', 'sass', 'personal'],
+			year: '2014',
+			description: "Find out details about you and your favorite languages' activities on Github."
 		},
 		{
 			name: 'New York Times Visual Search',
 			url: 'news-search',
-			tags: ['javascript', 'angular', 'node', 'express', 'html', 'css', 'sass', 'personal']
+			tags: ['javascript', 'angular', 'node', 'express', 'html', 'css', 'sass', 'personal'],
+			year: '2014',
+			description: 'Search the news with an easy to use visual interface.'
 		},
 		{
 			name: 'Wedding Website',
 			url: 'sally-joey-wedding',
-			tags: ['ux', 'ui', 'html', 'css', 'sass', 'personal']
+			tags: ['ux', 'ui', 'html', 'css', 'sass', 'personal'],
+			year: '2014',
+			description: "A responsive website designed and coded for dear friends' wedding."
 		},
 		{
 			name: "Migration of Elsevier Users to Inkling",
 			url: 'inkling-elsevier-migration',
-			tags: ['ux', 'ui', 'user testing', 'html', 'css', 'sass', 'professional']
+			tags: ['ux', 'ui', 'user testing', 'html', 'css', 'sass', 'professional'],
+			year: '2013-2014',
+			description: 'A UX and UI flow to move over a million users to a new platform.'
 		},
 		{
 			name: "Inkling's Whitelabeled Store for Elsevier",
 			url: 'inkling-elsevier-whitelabel',
-			tags: ['ux', 'ui', 'user testing', 'html', 'css', 'sass', 'professional']
+			tags: ['ux', 'ui', 'user testing', 'html', 'css', 'sass', 'professional'],
+			year: '2013-2014',
+			description: 'A cobranded experience between an international textbook company and a smartly designed eBook product.'
 		},
 		{
 			name: 'Inkling Store Product Details',
 			url: 'inkling-product-details',
-			tags: ['ux', 'ui', 'user testing', 'html', 'css', 'sass', 'professional']
+			tags: ['ux', 'ui', 'user testing', 'html', 'css', 'sass', 'professional'],
+			year: '2014',
+			description: 'A redesign of one of the core pages on the Inkling web store.'
 		},
 		{
 			name: 'Inkling for Web Style Guide',
 			url: 'inkling-style-guide',
-			tags: ['ui', 'user testing', 'html', 'css', 'workflow', 'professional']
+			tags: ['ui', 'user testing', 'html', 'css', 'workflow', 'professional'],
+			year: '2013-2014',
+			description: "A mission to unify the visual styling across all of Inkling's web presences."
 		}
 
 	];
---
+++ client/project/projectTemplates/inkling-product-details/inkling-product-details.js
@@ -0,0 +1,71 @@
+angular.module('portfolio.projects')
+
+	.controller('InklingProductDetailsController', function ($scope, ProjectData, Lightbox) {
+
+		// Get project info
+		ProjectData.forEach(function (value, index) {
+			if(value.url === 'inkling-product-details') {
+				$scope.projectInfo = value;
+			}
+		})
+
+		$scope.disparateStyles = [
+
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/open-inkling-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/open-inkling-thumb.jpg'
+			},
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/inkling-web-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/inkling-web-thumb.jpg'
+			},
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/chapter-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/chapter-thumb.jpg'
+			},
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/reader-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/reader-thumb.jpg'
+			}
+		];
+
+		$scope.buttons = [
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/buttons-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/buttons-thumb.jpg'
+			}
+		];
+
+		$scope.dropbox = [
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/form-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/form-thumb.jpg'
+			},
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/text-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/text-thumb.jpg'
+			}
+		];
+
+		$scope.final = [
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/open-inkling-final-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/open-inkling-final-thumb.jpg'
+			},
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/homepage-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/homepage-thumb.jpg'
+			},
+			{
+				'url': 'project/projectTemplates/inkling-style-guide/images/reader-final-full.jpg',
+				'thumbUrl': 'project/projectTemplates/inkling-style-guide/images/reader-final-thumb.jpg'
+			}
+		];
+
+
+
+
+		$scope.openLightboxModal = function(groupname, index) {
+			Lightbox.openModal($scope[groupname], index)
+		}
+	})
