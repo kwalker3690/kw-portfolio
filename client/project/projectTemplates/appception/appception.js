20f1a00f93f20676cf8c897a23c4a9cf7a2b5896
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-10-26 13:53:30 -0700

    appception done

--- client/project/projectTemplates/appception/appception.js
+++ client/project/projectTemplates/appception/appception.js
@@ -38,6 +38,33 @@ angular.module('portfolio.projects')
 			}
 		];
 
+		$scope.recursive = [
+			{
+				'url': 'project/projectTemplates/appception/images/finder-file-full.jpg',
+				'thumbUrl': 'project/projectTemplates/appception/images/finder-file-thumb.jpg',
+				'caption': 'The file structure as seen in Finder.',
+				'shortCaption': 'The file structure as seen in Finder.'
+			},
+			{
+				'url': 'project/projectTemplates/appception/images/directive-1-full.jpg',
+				'thumbUrl': 'project/projectTemplates/appception/images/directive-1-thumb.jpg',
+				'caption': 'The outer directive to create unordered lists.',
+				'shortCaption': 'The outer directive to create unordered lists.'
+			},
+			{
+				'url': 'project/projectTemplates/appception/images/directive-2-full.jpg',
+				'thumbUrl': 'project/projectTemplates/appception/images/directive-2-thumb.jpg',
+				'caption': 'The inner directive that does the bulk of the work. It checks to see if the input provided has children. If it does, it creates a new unordered list as a list item using the outer directive. If not, it adds a plain list item to the current unordered list.',
+				'shortCaption': 'The inner directive that does the bulk of the work. . .'
+			},
+			{
+				'url': 'project/projectTemplates/appception/images/final-file-full.jpg',
+				'thumbUrl': 'project/projectTemplates/appception/images/final-file-thumb.jpg',
+				'caption': 'The resulting output, in rendered HTML and CSS.',
+				'shortCaption': 'The resulting output, in rendered HTML and CSS.'
+			}
+		];
+
 
 
 
