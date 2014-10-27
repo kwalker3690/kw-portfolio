472cd473e89263d93a7438774bd83319fb6cce7b
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-10-26 18:35:50 -0700

    github scout complete

---
+++ client/project/projectTemplates/github-scout/github-scout.js
@@ -0,0 +1,31 @@
+angular.module('portfolio.projects')
+
+	.controller('GithubScoutController', function ($scope, ProjectData, Lightbox) {
+
+		// Get project info
+		ProjectData.forEach(function (value, index) {
+			if(value.url === 'github-scout') {
+				$scope.projectInfo = value;
+			}
+		})
+
+		$scope.user = [
+
+			{
+				'url': 'project/projectTemplates/github-scout/images/user-graph-full.jpg',
+				'thumbUrl': 'project/projectTemplates/github-scout/images/user-graph-thumb.jpg'
+			}
+		];
+
+		$scope.language = [
+			{
+				'url': 'project/projectTemplates/github-scout/images/lang-graph-full.jpg',
+				'thumbUrl': 'project/projectTemplates/github-scout/images/lang-graph-thumb.jpg'
+			}
+		];
+
+
+		$scope.openLightboxModal = function(groupname, index) {
+			Lightbox.openModal($scope[groupname], index)
+		}
+	})
